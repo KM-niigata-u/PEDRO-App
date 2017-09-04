b58374b06f7545e37ae84a5b96b6341e49ebf699
KM-niigata-u <f16c032b@mail.cc.niigata-u.ac.jp>
2017-09-04 14:47:34 +0900

    first commit

---
+++ routes/field-data.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var csv = require('csv');
+var fs = require('fs');
+
+var router = express.Router();
+
+var filename = "./ped/field_2017.csv";
+var parse = csv.parse({columns: true});
+var readableStream = fs.createReadStream(filename, {encoding: 'utf-8'});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  var data = [];
+
+  readableStream.pipe(parse)
+
+  parse.on('readable', () => {
+    var tmp;
+    while(tmp = parse.read()) {
+      data.push(tmp);
+    }
+  });
+
+  parse.on('end', () => {
+    console.log(data);
+    res.send(data);
+  })
+
+
+
+});
+
+module.exports = router;
